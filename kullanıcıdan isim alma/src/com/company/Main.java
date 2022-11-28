5d3a122b9e71d23fb5012b8f2d26ccf0f2427ee3
berk cirak <atalayberkcrak@gmail.com>
2022-11-28 21:29:17 +0300

    kullanicidan isim alma
    kullanici ismini girer

---
+++ kullanıcıdan isim alma/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+    System.out.println("isminizi giriniz: ");
+    String isim = scan.nextLine();
+    System.out.println("girilen isim: " + isim);
+
+    }
+}
